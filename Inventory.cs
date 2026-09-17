e667dc16abee9939ed5507a423c2d6a15dfdb444
Himani-y <yhimani2211@gmail.com>
2026-09-17 21:26:45 +0530

    Initial commit -Moduar Inventory SYstem

---
+++ Inventory.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem
+{
+    class Inventory
+    {
+        private List<Item> items = new List<Item>();
+        public const int MaxInventorySlots = 20;
+        public void AddItem(Item item, int number)
+        {
+            foreach(Item i in items)
+            {
+                if(i.Name == item.Name)
+                {
+                    i.AddQuantity(number);
+                    return;
+                }
+               
+            }
+            item.AddQuantity(number);
+            items.Add(item);
+        }
+
+        public void DisplayItem()
+        {
+            foreach (Item itm in items)
+            {
+                Console.WriteLine(itm.Name);
+                Console.WriteLine(itm.Quantity);
+            }
+        }
+
+        public bool RemoveItem(Item item, int num)
+        {
+            Item? itemtoremove = null;
+            Item? itemF = FindItem(item.Name);
+            if(itemF!= null)
+            {
+                if(itemF.RemoveQuantity(num))
+                { 
+                    if (itemF.Quantity <= 0)
+                    {
+                        itemtoremove = itemF;
+                    }
+                       
+                }
+                else
+                {
+                    Console.WriteLine("Not enough quantity to remove");
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Item not found ");
+                return false;
+            }
+
+            if (itemtoremove != null)
+            {
+                items.Remove(itemtoremove);
+                
+            }
+            return true;
+        }
+
+        public bool HasItem(Item item)
+        {
+            Item itemF = FindItem(item.Name);
+            if(itemF != null)
+            {
+                Console.WriteLine("Player has " + item.Name);
+                return true;
+            }
+            Console.WriteLine("Player don' t have " + item.Name);
+            return false;
+        }
+
+        public int GetItemQuantity(Item item)
+        {
+            Item? itemF = FindItem(item.Name);
+
+            if (itemF != null)
+            {
+                return itemF.Quantity;
+            }
+                
+            
+            return 0;
+        }
+
+        private Item? FindItem(String ItemName)
+        {
+            foreach(Item a in items)
+            {
+                if (a.Name == ItemName)
+                    return a;
+            }
+            return null;
+        }
+
+        public static int inventoryCount = 0;
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using InventorySystem;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public class Item {
+
+        private string name = "";
+        private int quantity;
+        public static int TotalItemsCreated = 0;
+
+        public Item()
+        {
+            TotalItemsCreated++;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Quantity
+        {
+            get { return quantity; }
+           
+        }
+
+    public void AddQuantity(int amount)
+    {
+        if (amount > 0)
+            quantity += amount;
+        else
+            return;
+    }
+
+    public bool RemoveQuantity(int amount)
+    {
+        if(amount <= quantity && amount > 0)
+        {
+            quantity -= amount;
+            return true;
+        }
+        return false;
+
+    }
+}
+    class Program
+    {
+        static void Main()
+        {
+            Item potion = new Item();
+
+            potion.Name = "Health Potion";
+            potion.AddQuantity(2);
+
+            Item weapon = new Item();
+
+            weapon.Name = "Sword";
+            weapon.AddQuantity(3);
+            Item Gun = new Item();
+
+            Gun.Name = "gun";
+            Gun.AddQuantity(4);
+
+            Inventory inventory = new Inventory();
+            inventory.AddItem(potion, 2);
+            inventory.AddItem(weapon, 3);
+            inventory.AddItem(potion, 1);
+            inventory.DisplayItem();
+           bool success =  inventory.RemoveItem(potion, 7);
+           bool success1 = inventory.RemoveItem(Gun, 7);
+            bool success3 = inventory.HasItem(potion);
+            inventory.HasItem(Gun);
+        inventory.DisplayItem();
+        Console.WriteLine("Potion removed" + success);
+        Console.WriteLine("Gun removed"+ success1);
+        Console.WriteLine("found potion or not"+ success3);
+
+        Console.WriteLine(Inventory.MaxInventorySlots);
+
+        Console.Write("Enter quantity: ");
+        string input = Console.ReadLine();
+
+        try
+        {
+            int quantity = int.Parse(input);
+            Console.WriteLine("Quantity: " + quantity);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please enter a valid number.");
+        }
+        Console.ReadLine();
+        }
+    }
+    
+
+
